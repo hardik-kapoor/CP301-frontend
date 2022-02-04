5eb91a9b12f5629813181a3e40ce66f20a4c694a
Hardik Kapoor <2019mcb1220@iitrpr.ac.in>
2022-02-04 15:22:21 +0530

    google auth working

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,36 +1,35 @@
-import React, { useEffect } from "react";
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
-const GoogleAuth = props => {
-    const onAuthChange = isSignedIn => {
+class GoogleAuth extends Component{
+    onAuthChange = isSignedIn => {
         if (isSignedIn)
-            props.signIn(auth.currentUser.get().getId())
+            this.props.signIn(this.auth.currentUser.get().getId())
         else
-            props.signOut();
+            this.props.signOut();
     };
 
-    let auth=null;
-
-    useEffect(() => {
+    componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: "971254412778-5u87cu758seqpe71o0r4tqp100q7qfpl.apps.googleusercontent.com",
                 scope: "email"
             }).then(() => {
-                auth = window.gapi.auth2.getAuthInstance();
-                onAuthChange(auth.isSignedIn.get());
-                auth.isSignedIn.listen(onAuthChange);
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.onAuthChange(this.auth.isSignedIn.get());
+                this.auth.isSignedIn.listen(this.onAuthChange);
             })
         })
-    }, []);
+    }
 
-    const renderAuthButton = () => {
-        if (props.isSignedIn === null)
+    renderAuthButton = () => {
+        if (this.props.isSignedIn === null)
             return null
-        if (props.isSignedIn) {
+        if (this.props.isSignedIn) {
             return (
-                <button className="ui red button" onClick={() => auth.signOut()}>
+                <button className="ui red button" onClick={() => this.auth.signOut()}>
                     <i className="google icon"></i>
                     Sign Out
                 </button>
@@ -38,7 +37,7 @@ const GoogleAuth = props => {
         }
         return (
             <>
-                <button className="ui inverted button" onClick={() => auth.signIn()} style={{ marginRight: '0.5em' }}>
+                <button className="ui inverted button" onClick={() => this.auth.signIn()} style={{ marginRight: '0.5em' }}>
                     <i className="google icon"></i>
                     Log In with Google
                 </button>
@@ -47,8 +46,10 @@ const GoogleAuth = props => {
         );
     };
 
-    return <>{renderAuthButton()}</>;
-};
+    render(){
+        return <>{this.renderAuthButton()}</>;
+    }
+}
 
 const mapStateToProps = state => {
     return { isSignedIn: state.auth.isSignedIn };
