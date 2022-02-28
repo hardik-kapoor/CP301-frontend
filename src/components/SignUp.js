176fe2bda856f5426b3e51e471727538e4da98b3
Hardik Kapoor <2019mcb1220@iitrpr.ac.in>
2022-02-28 22:30:52 +0530

    fixed rerendering

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import { Field, reduxForm } from 'redux-form';
+import { Field, formValues, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import Header from './Header';
 import './styles/SignUp.css';
 
-const SignUp = props => {
-    console.log(props);
-
-    const renderInput = formProps =>{
+class SignUp extends React.Component{
+    
+    renderInput = formProps =>{
         return(
             <div className='field'>
                 <label>{formProps.label}</label>
@@ -18,37 +16,40 @@ const SignUp = props => {
             </div>
         );
     };
-
-    const onSubmit = (formValues) => {
-        props.onFormSubmit(formValues);
+    
+    onSubmit = (formValues) => {
+        this.props.onFormSubmit(formValues);
     };
+    
+    render(){
+        console.log(this.props);
+        return (
+            <>
+                <Header />
+                <div className='center'>
+                    <h2 style={{ textAlign: 'center' }}>Sign up</h2>
+                    <form className='ui form error' onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                        <h3 className='ui dividing header'>Basic Information</h3>
+                        <Field name="username" type='text' label="User Name" component={this.renderInput}></Field>
+                        <Field name="email_id" type='text' label="Email ID" component={this.renderInput}></Field>
+                        <Field name="password" type='password' label="Password" component={this.renderInput}></Field>
+                        <Field name="re_enter_password" type='password' label="Re Enter Password" component={this.renderInput}></Field>
+                        <button className='ui primary button'>Submit</button>
+                    </form>
+                </div>
+            </>
+        );
+    }
+};
 
-    return (
-        <>
-            <Header />
-            <div className='center'>
-                <h2 style={{ textAlign: 'center' }}>Sign up</h2>
-                <form className='ui form error' onSubmit={props.handleSubmit(onSubmit)}>
-                    <h3 className='ui dividing header'>Basic Information</h3>
-                    <Field name="username" type='text' label="User Name" component={renderInput}></Field>
-                    <Field name="email_id" type='text' label="Email ID" component={renderInput}></Field>
-                    <Field name="password" type='password' label="Password" component={renderInput}></Field>
-                    <Field name="re_enter_password" type='password' label="Re Enter Password" component={renderInput}></Field>
-                    <div className='field'>
-                        <label>temp</label>
-                        <div className='field'>
-                            <input type='text' name='text'/>
-                        </div>
-                    </div>
-                    <button className='ui primary button'>Submit</button>
-                </form>
-            </div>
-        </>
-    );
+const validate = formValues => {
+    const errors={};
+    
 };
 
 const formWrapped = reduxForm({
-    form: 'sign_in_form'
+    form: 'sign_in_form',
+    validate
 })(SignUp);
 
 export default connect(null)(formWrapped);
