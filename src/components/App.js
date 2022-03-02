1ac62b9f4e1c25abfc0dc220a8eac9d35089ddaa
Hardik Kapoor <2019mcb1220@iitrpr.ac.in>
2022-03-02 17:03:24 +0530

    combining login and signup to auth

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { Router, Switch, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import BookExchange from './BookExchange/BookExchange';
 import SignUp from './SignUp';
+import Auth from './Auth';
 import history from '../history';
 import LogIn from './LogIn';
 
@@ -26,10 +27,10 @@ const App = () => {
                         <BookExchange />
                     </Route>
                     <Route path='/signup'>
-                        <SignUp />
+                        <Auth type='Sign Up'/>
                     </Route>
                     <Route path='/login'>
-                        <LogIn />
+                        <Auth type='Log In'/>
                     </Route>
                 </Switch>
             </Router>
