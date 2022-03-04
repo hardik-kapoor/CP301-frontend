b685b681bdac4f9ea517b0ee1934b9e2e7057e77
Hardik Kapoor <2019mcb1220@iitrpr.ac.in>
2022-03-05 04:42:23 +0530

    added relevant course functionality

--- src/components/BookExchange/CreateBook.js
+++ src/components/BookExchange/CreateBook.js
@@ -4,7 +4,7 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 
 class CreateBook extends React.Component{
-    
+    state={counter:0,elem:[]};
 
     renderInput = formProps =>{
         return (
@@ -16,6 +16,27 @@ class CreateBook extends React.Component{
         );
     };
 
+    renderRelevantCourseInput = () =>{
+        console.log(this.state);
+        this.setState((prev)=>{
+            return {
+                    counter:prev.counter+1,
+                    elem:[...prev.elem,
+                        <div className='three fields' key={prev.counter+1}>
+                            <Field name={`course_code_${prev.counter+1}`} type='text' label="Course Code" component={this.renderInput} />
+                            <Field name={`course_name_${prev.counter+1}`} type='text' label="Course Name" component={this.renderInput} />
+                            <Field name={`course_dept_${prev.counter+1}`} type='text' label="Department" component={this.renderInput} />
+                        </div>  
+                    ]
+                }
+            }
+        );
+    };
+
+
+    onSubmit = formValues => {
+        console.log(formValues);
+    }
 
     render(){
         const srcc='https://www.publicbooks.org/wp-content/uploads/2017/01/book-e1484158615982.jpg';
@@ -33,19 +54,19 @@ class CreateBook extends React.Component{
                         </div>
                     </div>
                     <div className='eight wide column p-5'>
-                        <form className='ui form error'>
+                        <form className='ui form error' onSubmit={this.props.handleSubmit(this.onSubmit)}>
                             <h2 className='ui dividing header'>Book Details</h2>
                             <Field name='BookName' type='text' label="Book Name" component={this.renderInput} />
                             <Field name='BookAuthor' type='text' label="Book Author" component={this.renderInput} />
                             <Field name='BookType' type='text' label="Book Type" component={this.renderInput} />
-                            <Field name='Cost' type='number' label="Cost" component={this.renderInput} />
                             <Field name='Description' type='text' label="Description" component={this.renderInput} />
-                            <label className='ui header'>Relevant Courses</label>   
-                            <div className='three fields'>
-                                <Field name='course_code_1' type='text' label="Course Code" component={this.renderInput} />
-                                <Field name='course_name_1' type='text' label="Course Name" component={this.renderInput} />
-                                <Field name='course_dept_1' type='text' label="Department" component={this.renderInput} />
+                            <Field name='Cost' type='number' label="Cost" component={this.renderInput} />
+                            <div style={{cursor:'pointer'}} onClick={()=>this.renderRelevantCourseInput()}>
+                                <label className='ui header'>Relevant Courses</label>  
+                                <i className="plus circle icon large float-end"></i>
                             </div>
+                            {this.state.elem}
+                            <button className='d-block ui button blue'>Submit</button>
                         </form>
                     </div>
                 </div>
